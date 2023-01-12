36c896099e200602d2c71b9794b228f323bcadcf
LucaBassi <l.bassi@hotmail.fr>
2023-01-12 09:32:48 +0100

    Update ps.ps1

--- PS/ps.ps1
+++ PS/ps.ps1
@@ -1,5 +1,8 @@
-param ($servername)
-if ($servername -eq $null) {
-    $servername = read-host -Prompt "Please enter a servername" 
-}
-write-host "If this script were really going to do something, it would do it on $servername" 
+# Affiche tous les fichiers et dossiers de l'arborescence passee en parametre qui sont plus vieux de 6 mois ou du nombre de mois passe en parametre
+#
+# Pascal Hurni, novembre 2011
+
+Param ($Folder = ".", $MonthCount = 6)
+
+Get-ChildItem -Recurse $Folder | 
+  Where-Object {$_.LastWriteTime -lt (Get-Date).AddMonths(-$MonthCount) }
